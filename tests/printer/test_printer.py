c624ed797003911c6cf14330082fd55eb22058b4
KatoRyota <kato.ryota.0124@gmail.com>
2021-08-18 20:33:50 +0900

    初回コミット。

--- tests/printer/test_printer.py
+++ tests/printer/test_printer.py
@@ -122,6 +122,42 @@ class TestPrinter(TestCase):
 '''
             self.assertRegexpMatches(actual, expected)
 
+        # ---- ケース4.1 ----
+        with mock.patch("sys.stdout", new=BytesIO()) as stdout:
+            # 前提条件
+            context = Context()
+            context.project_dir = os.path.join("dst_dir", "project")
+            context.success_repositories = ["db-client", "git-synchronizer"]
+            context.fail_repositories = ["experimental-tools"]
+            context.stash_repositories = ["db-client", "git-synchronizer"]
+
+            # 実行
+            Printer(context).execute()
+
+            # 検証
+            actual = stdout.getvalue().decode("utf-8")
+            expected = u'''\
+-------------------------------------------------------------------------------
+【結果レポート】
+
+同期に失敗したリポジトリがあります。
+以下のディレクトリ配下のリポジトリの同期を行いました。
+.*dst_dir.project
+
+--- ワーキングディレクトリの内容を、Git Stashに保存したリポジトリ ---
+　　・db-client
+　　・git-synchronizer
+
+--- 同期に失敗したリポジトリ ---
+　　・experimental-tools
+
+--- 同期に成功したリポジトリ ---
+　　・db-client
+　　・git-synchronizer
+
+'''
+            self.assertRegexpMatches(actual, expected)
+
 
 if __name__ == "__main__":
     unittest.main()
