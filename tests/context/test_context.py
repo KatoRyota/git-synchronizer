4e87a41d5d259fd18a0e4a0b3ea3a244124f3440
KatoRyota <kato.ryota.0124@gmail.com>
2021-08-19 14:55:48 +0900

    初回コミット。

---
+++ tests/context/test_context.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+from unittest import TestCase
+import unittest
+
+
+class TestContext(TestCase):
+
+    def test_check_application_initialize(self):
+        self.fail()
+
+    def test_check_option_parse(self):
+        self.fail()
+
+    def test_check_repo_file_load(self):
+        self.fail()
+
+    def test_check_synchronize(self):
+        self.fail()
+
+
+if __name__ == "__main__":
+    unittest.main()
